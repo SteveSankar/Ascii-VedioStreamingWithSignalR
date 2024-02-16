0bd6648bd3b48c03202e81ae9d3d2b98890cfa59
sankar <kollisankarrao00@gmail.com>
2024-02-16 23:14:16 +0530

    initial commit

---
+++ Hubs/StreamHub.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace StreamR
+{
+    public class StreamHub : Hub
+    {
+        private readonly StreamManager _streamManager;
+
+        public StreamHub(StreamManager streamManager)
+        {
+            _streamManager = streamManager;
+        }
+
+        public List<string> ListStreams()
+        {
+            return _streamManager.ListStreams();
+        }
+
+        public IAsyncEnumerable<string> WatchStream(string streamName, CancellationToken cancellationToken)
+        {
+            return _streamManager.Subscribe(streamName, cancellationToken);
+        }
+
+        public async Task StartStream(string streamName, IAsyncEnumerable<string> streamContent)
+        {
+            try
+            {
+                var streamTask = _streamManager.RunStreamAsync(streamName, streamContent);
+
+                // Tell everyone about your stream!
+                await Clients.Others.SendAsync("NewStream", streamName);
+
+                await streamTask;
+            }
+            finally
+            {
+                await Clients.Others.SendAsync("RemoveStream", streamName);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using StreamR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+builder.Services.AddSingleton<StreamManager>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapHub<StreamHub>("/stream");
+
+app.Run();
